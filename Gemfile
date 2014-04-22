f1afc7ca2e5a51d4a6b44f90e787abe71b82f223
Tyler Ewing <tewing10@gmail.com>
2014-04-21 22:14:39 -0400

    Automated MySQL install and fixed Gemfile

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem 'mysql'
+gem 'mysql2'
 gem 'petfinder', git: 'git://github.com/TeamDogFace/Petfinder.git'
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,15 @@ echo Installing curl
 apt-get install -y curl > /dev/null 2>&1
 echo Installing git
 apt-get install -y git > /dev/null 2>&1
+echo Installing and Configuring MySQL
+echo mysql-server mysql-server/root_password password pass | debconf-set-selections
+echo mysql-server mysql-server/root_password_again password pass | debconf-set-selections
+apt-get install -y mysql-server mysql-client libmysqlclient-dev > /dev/null 2>&1
 echo Downloading and Installing RVM
-curl -L https://get.rvm.io | bash -s stable --ruby > /dev/null
+curl -L https://get.rvm.io | bash -s stable > /dev/null
 source /usr/local/rvm/scripts/rvm
-echo "We want a Ruby version >2.0"
+rvm install ruby-2.0.0
+echo "We want a Ruby version == 2.0"
 echo `ruby -v`
 cd /vagrant
 echo Installing bundler gem
